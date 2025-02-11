6d1eaa3a09dd97caee9113ecd1ec4a0f2b15002e
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-11 07:40:47 +0000

    Updating to latest FlutterFlow output. Whoo\!

---
+++ lib/backend/firebase/firebase_config.dart
@@ -0,0 +1,18 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
+
+Future initFirebase() async {
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+            apiKey: "AIzaSyCWaHlqU2vzywiwhGm7yNOd0hPw54hmBEw",
+            authDomain: "to-do-c7osk4.firebaseapp.com",
+            projectId: "to-do-c7osk4",
+            storageBucket: "to-do-c7osk4.firebasestorage.app",
+            messagingSenderId: "878232797692",
+            appId: "1:878232797692:web:df77a3353240f4d159f279",
+            measurementId: "G-HQGESVQ7NT"));
+  } else {
+    await Firebase.initializeApp();
+  }
+}
---
+++ lib/onboarding/onboarding_model.dart
@@ -0,0 +1,31 @@
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/form_field_controller.dart';
+import 'onboarding_widget.dart' show OnboardingWidget;
+import 'package:flutter/material.dart';
+
+class OnboardingModel extends FlutterFlowModel<OnboardingWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  bool isDataUploading = false;
+  FFUploadedFile uploadedLocalFile =
+      FFUploadedFile(bytes: Uint8List.fromList([]));
+  String uploadedFileUrl = '';
+
+  // State field(s) for Name-profile widget.
+  FocusNode? nameProfileFocusNode;
+  TextEditingController? nameProfileTextController;
+  String? Function(BuildContext, String?)? nameProfileTextControllerValidator;
+  DateTime? datePicked;
+  // State field(s) for EggPref-Dropdown widget.
+  String? eggPrefDropdownValue;
+  FormFieldController<String>? eggPrefDropdownValueController;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    nameProfileFocusNode?.dispose();
+    nameProfileTextController?.dispose();
+  }
+}
